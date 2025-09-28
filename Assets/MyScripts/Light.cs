52145a422bf10e7f2b1416711d3bb42e12501b69
Melatxnin <n.gossard@rubika-edu.re>
2025-09-29 00:12:47 +0400

    update

--- Assets/MyScripts/Light.cs
+++ Assets/MyScripts/Light.cs
@@ -3,37 +3,18 @@ using UnityEngine;
 public class LightCycle : MonoBehaviour
 {
     private Renderer rend;
+    private Transform transf;
     
-    [SerializeField] private Color[] colors = 
-    {
-        Color.green, 
-        Color.black, 
-        Color.blue, 
-        Color.gray, 
-        Color.yellow, 
-        Color.red
-    };
-
-    [SerializeField] private float duration = 1f;
-    
-    private int index = 0;
-    private float timer = 0f;
 
     void Start()
     {
-        rend = GetComponent<Renderer>();
-        rend.material.color = colors[index];
+
     }
 
     void Update()
     {
-        timer += Time.deltaTime;
-
-        if (timer >= duration)
-        {
-            index = (index + 1) % colors.Length;
-            rend.material.color = colors[index];
-            timer = 0f;
-        }
+        float hauteur = transf.position.y;
+        float t = Mathf.InverseLerp(0, 10f, hauteur);
+        rend.material.color = Color.Lerp(Color.green, Color.red, t);
     }
 }
